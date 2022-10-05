1774c118ab7714585816f52a4555c658643e3fc6
Dominik Chrastecky <dominik@chrastecky.cz>
2022-10-05 15:42:52 +0200

    Add transifex config

---
+++ translations-other/harbour-bitsailor.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DesktopFile</name>
+    <message>
+        <source>BitSailor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PolkitFile</name>
+    <message>
+        <source>Authorization is needed to unlock the vault.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
