aa70fd0b5f9461077feb193c7a8f46bc04a44ae8
Dominik Chrastecky <dominik@chrastecky.cz>
2022-10-06 18:47:26 +0200

    Add comments for translations-other

--- translations-other/harbour-bitsailor.ts
+++ translations-other/harbour-bitsailor.ts
@@ -5,6 +5,7 @@
     <name>DesktopFile</name>
     <message>
         <source>BitSailor</source>
+		<extracomment>Displayed in the app grid as application name</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -12,6 +13,7 @@
     <name>PolkitFile</name>
     <message>
         <source>Authorization is needed to unlock the vault.</source>
+		<extracomment>Displayed in system dialog when system auth (fingerprint auth) is enabled</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
